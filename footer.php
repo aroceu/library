a5b5a63cc953c2b73aa417a2f35e6987548b6a38
Arrow Sparrow <aroceu@gmail.com>
2024-10-23 23:23:59 -0400

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
  <div class="footer">
-        	<a href="https://kingdra.net">my clubhouse</a> <img src="img/moon.gif" alt="">  <a href="/docs">documentation</a> <img src="img/sparkle.gif" alt="">  <br />
+        	<a href="https://github.com/aroceu/library">view on github</a> <img src="img/moon.gif" alt=""> <a href="https://kingdra.net">my clubhouse</a>  <img src="img/sparkle.gif" alt=""> <a href="/docs">documentation</a> <br />
         &copy; 2024 aroceu <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com">contact</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/fic">fic</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/generators">prompt generators</a>
     </div>
 

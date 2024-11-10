1040e90bd6dd796b11322484347978706a08b2cf
Arrow Sparrow <dev@aroceu.cloud>
2024-11-10 01:37:27 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
  <div class="footer">
-        	<a href="https://github.com/aroceu/library">view on github</a> <img src="img/moon.gif" alt=""> <a href="https://kingdra.net">my clubhouse</a>  <img src="img/sparkle.gif" alt=""> <a href="/docs">documentation</a> <br />
-        &copy; 2024 aroceu <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com">contact</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/fic">fic</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/generators">prompt generators</a>
+        	<a href="https://github.com/aroceu/library">view on github</a> <img src="img/moon.gif" alt=""> <a href="/docs">documentation</a> <img src="img/sparkle.gif" alt=""> <br />
+        &copy; 2024 <a href="https://aroceu.com">aroceu</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/fic">fic</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/generators">prompt generators</a> <img src="img/ribbon.gif" alt=""> <a href="https://kingdra.net">portal</a>
     </div>
 
         </div>

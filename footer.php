67a65e8d75118a38eb5366acfa21371abdbc0b32
Arrow Sparrow <dev@aroceu.cloud>
2024-12-08 22:18:20 -0500

    Added sitemap in footer

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
  <div class="footer">
-        	<a href="https://github.com/aroceu/library">view on github</a> <img src="img/moon.gif" alt=""> <a href="/docs">documentation</a> <img src="img/sparkle.gif" alt=""> <br />
+        	<a href="https://github.com/aroceu/library">view on github</a> <img src="img/moon.gif" alt=""> <a href="/docs">docs</a> <img src="img/sparkle.gif" alt=""> <a href="/atlas">sitemap</a><br />
         &copy; 2024 <a href="https://aroceu.com">aroceu</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/fic">fic</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/generators">prompt generators</a> <img src="img/ribbon.gif" alt=""> <a href="https://kingdra.net">portal</a>
     </div>
 

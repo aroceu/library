6a752efb99e5ba9abbde91535629d05e951a7926
Arrow Sparrow <dev@aroceu.cloud>
2024-12-25 18:13:48 -0800

    Added live show archive

---
+++ live.php
@@ -0,0 +1,54 @@
+<html lang="en">
+    
+    <head>
+        
+        <title>aroceu's library (live shows)</title>
+        
+        <script type="text/javascript">// <![CDATA[
+		function ShowHide(divId)
+		{
+		if(document.getElementById(divId).style.display == 'none')
+		{
+		document.getElementById(divId).style.display='block';
+		}
+		else
+		{
+		document.getElementById(divId).style.display = 'none';
+		}
+		}
+	// ]]></script>
+        
+ <?php include('header.php');?>
+
+	<h1>aroceu's live show archive</h1>
+
+            <div id="blurb"><p>This section is more out of the way because I'm not really the type of person who goes to live shows; it tends to be more of an ~event when I do. Below is an archive of all the different types entertainment that I've seen live, from concerts to musicals to comedy shows.</p>
+
+                <p>I also used to be super involved in my old church community so I've been to a handful of Christian concerts as well, but I don't remember those years/dates or the groups anymore. I will say I've seen Hillsong United live, though.</p>
+
+                <blockquote><mg></mg> also did a meet & greet</blockquote>
+
+                </div>
+
+            <ul id="archivelist">
+
+                <li><em>My Brother, My Brother, and Me</em> &bull; <strong>September 2024</strong> &bull; Atlanta Symphony Hall @ Atlanta, GA</li>
+                <li><em>Walk The Moon</em> &bull; <strong>September 2021</strong> &bull; Buckhead Theatre @ Atlanta, GA</li>
+                <li><mg></mg> <em>Ryan Ross & Z Berg & Palm Springsteen & Dan Keyes</em> &bull; <strong>October 2019</strong> &bull; Three Links @ Dallas, TX</li>
+                <li><em>GWSN</em> &bull; <strong>November 2018</strong> &bull; Carrollton, TX</li>
+                <li><em>Panic! at the Disco & Hayley Kiyoko & Max Frost</em> &bull; <strong>August 2018</strong> &bull; AAC @ Dallas, TX</li>
+                <li><em>Les Miserables</em> &bull; <strong>July 2016</strong> &bull; Imperial Theatre @ New York City, NY</li>
+                <li><em>One Direction</em> &bull; <strong>September 2015</strong> &bull; Lincoln Financial Field @ Philadelphia, PA</li>
+                <li><mg></mg> <em>Paradise Fears & Against the Current & William Beckett</em> &bull; <strong>August 2014</strong> &bull; The Music Hall at World Cafe Live @ Philadelphia, PA </li>
+                <li><em>Panic! at the Disco & Walk The Moon & Magic Man</em> &bull; <strong>July 2014</strong> &bull; Penn's Landing @ Philadelphia, PA</li>
+                <li><em>Fall Out Boy & Paramore & New Politics</em> &bull; <strong>July 2014</strong> &bull; Hershey Park @ Hershey, PA</li>
+                <li><em>One Direction</em> &bull; <strong>August 2013</strong> &bull; New York City on the Today Show, NY</li>
+                <li><em>Panic! at the Disco & Walk off the Earth & Alt-J</em> &bull; <strong>August 2013</strong> &bull; The Piazza at Schmidt's @ Philadelphia, PA</li>
+                <li><em>SMTown 2011</em> &bull; <strong>November 2011</strong> &bull; Madison Square Garden @ New York City, NY</li>
+                <li><em>Blink-182 & Fall Out Boy & Panic! at the Disco</em> &bull; <strong>August 2009</strong> &bull; Hershey Park @ Hershey, PA</li>
+                <li><em>All Time Low & We The Kings & Cartel & Days Difference</em> &bull; <strong>July 2009</strong> &bull; Electric Factory @ Philadelphia, PA</li>
+                <li><em>Jonas Brothers</em> &bull; <strong>August 2007</strong> &bull; Hershey Park @ Hershey, PA</li>
+            </ul>
+
+	        
+	         <?php include('footer.php');?>

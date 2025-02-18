0c76f2981f2fe506a813aa3599b9d2aafaa87c8e
Arrow Sparrow <dev@aroceu.cloud>
2025-02-18 02:16:19 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
  <div class="footer">
         	<a href="https://github.com/aroceu/library">view on github</a> <img src="img/moon.gif" alt=""> <a href="/docs">docs</a> <img src="img/sparkle.gif" alt=""> <a href="/atlas">sitemap</a><br />
         &copy; 2024 <a href="https://aroceu.com">aroceu</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/fic">fic</a> <img src="img/ribbon.gif" alt=""> <a href="https://aroceu.com/generators">prompt generators</a> <img src="img/ribbon.gif" alt=""> <a href="https://kingdra.net">portal</a>
-        <br />↑ ↑ ↓ ↓ ← → ← → B A
+        <br /><span style="letter-spacing: 10px">↑ ↑ ↓ ↓ ← → ← → B A</span>
     </div>
 
         </div>
